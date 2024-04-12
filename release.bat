e1b6912450e27601223fb18dc11ebd7b9e25df6d
Chang Xiao <chang.x1994@gmail.com>
2024-04-11 23:14:11 -0400

    Removed debug flag

--- release.bat
+++ release.bat
@@ -1,3 +1,3 @@
 @REM builds version without console popup
 @REM TODO: bundle dlls
-g++ -fdiagnostics-color=always -g **.cpp -o .\release\main.exe -lmingw32 -lSDL2main -lSDL2 -lSDL2_ttf -mwindows
+g++ -fdiagnostics-color=always -O2 **.cpp -o .\release\main.exe -lmingw32 -lSDL2main -lSDL2 -lSDL2_ttf
