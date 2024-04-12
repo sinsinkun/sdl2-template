06548b83e4f9183d1f023ae129d5d1126a4e48c0
Chang Xiao <chang.x1994@gmail.com>
2024-04-11 22:46:12 -0400

    Reverted release.bat

--- release.bat
+++ release.bat
@@ -1,3 +1,3 @@
 @REM builds version without console popup
+@REM TODO: bundle dlls
 g++ -fdiagnostics-color=always -g **.cpp -o .\release\main.exe -lmingw32 -lSDL2main -lSDL2 -lSDL2_ttf -mwindows
-echo "Completed"
